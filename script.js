4e952c5be435d5446a1fed9f575030fb07c30dcb
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-06-19 04:47:17 +0200

    feat: passer à la question suivante avec bouton et/ou miniteur

--- script.js
+++ script.js
@@ -1,7 +1,96 @@
 const acc = document.getElementById("submit");
 let quiz = document.getElementById("quiz");
+let countProgress = 100;
+
 let boutonsRadioDiv = document.querySelectorAll(".radio-bouton label");
 let selected = document.querySelectorAll('input[type="radio"]');
+let idQuestion = 1;
+let nombreQuestion = document.querySelector(".progress-questions");
+
+//Enoncé de la question
+let idEnonce = document.getElementById("enonce");
+
+//Les assertions
+
+let ass1 = document.getElementById("ass1");
+let ass2 = document.getElementById("ass2");
+let ass3 = document.getElementById("ass3");
+let ass4 = document.getElementById("ass4");
+
+//Questions
+const questions = [
+  {},
+  {},
+  {
+    enonce: "Comment faire appelle à une fonction nommée « sum »?",
+    assertions: [
+      "sum()",
+      "call function sum()",
+      "call sum()",
+      "Aucune réponse.",
+    ],
+    reponse: "sum()",
+  },
+
+  {
+    enonce:
+      "Quelle est la syntaxe correcte pour faire référence à un script externe appelé « myscript.js »??",
+    assertions: [
+      'script href=myscript.js"',
+      'script name="myscript.js"',
+      'script src="myscript.js"',
+      "Tout les réponses sont vrais",
+    ],
+    reponse: '<script src="myscript.js">',
+  },
+
+  {
+    enonce: "Comment écrire une condition IF en JavaScript?",
+    assertions: ["if a = 2 then", "if a = 2", "if a == 2 else", "if (a == 2)"],
+    reponse: "if (a == 2)",
+  },
+
+  {
+    enonce:
+      "Comment écrire une condition IF pour vérifier si « a » n’est PAS égal à 2?",
+    assertions: ["if a <> 2", "if (a != 2)", "if a =! 2 then", "if (a <> 2)"],
+    reponse: "if (a != 2)",
+  },
+];
+
+//Boucle réinitialiser les radio-boutons
+
+function reset() {
+  for (let i = 0; i < selected.length; i++) {
+    boutonsRadioDiv[i].style.border = "1px solid #bbb8b8";
+    boutonsRadioDiv[i].style.fontWeight = "400";
+    boutonsRadioDiv[i].style.color = "";
+  }
+}
+
+function questionSuivant() {
+  idQuestion++;
+  countProgress = 101;
+  nombreQuestion.innerHTML = "Question " + idQuestion + "/15";
+  suivant.disabled = true;
+  for (let i = 0; i < selected.length; i++) {
+    selected[i].checked = false;
+  }
+
+  reset();
+  idEnonce.innerHTML = questions[idQuestion].enonce;
+
+  ass1.innerHTML = questions[idQuestion].assertions[0];
+  ass2.innerHTML = questions[idQuestion].assertions[1];
+  ass3.innerHTML = questions[idQuestion].assertions[2];
+  ass4.innerHTML = questions[idQuestion].assertions[3];
+}
+
+//Passer à la question suivante
+suivant.addEventListener("click", function (e) {
+  e.preventDefault();
+  questionSuivant();
+});
 
 acc.addEventListener("click", function (e) {
   e.preventDefault();
@@ -9,43 +98,32 @@ acc.addEventListener("click", function (e) {
   quiz.style.display = "flex";
 });
 
-let countProgress = 100;
+//ProgressBar Timer
+
 let progressFont = document.querySelector(".font-progress-bar");
 setInterval(() => {
   if (countProgress >= 0 && quiz.style.display == "flex") {
     progressFont.style.width = countProgress + "%";
     progressTime.innerHTML = Math.floor(countProgress / 1.666666667);
     countProgress--;
-    if (countProgress < 17) {
-      progressTime.style.color = "red";
-      progressFont.style.backgroundColor = "red";
-    }
+  }
+  if (countProgress < 0) {
+    questionSuivant();
   }
 }, 600);
 
-// boutonsRadioDiv.forEach((div, idx) => {
-//   div.addEventListener("click", function (e) {
-//     boutonsRadioDiv[idx].style.border = "1px solid #028A3D";
-//     boutonsRadioDiv[idx].style.color = "#028A3D";
-//     boutonsRadioDiv[idx].style.fontWeight = "600";
-//   });
-// });
+//Changer le style de bordure pour le choix sélectionné
 
-let border = document.querySelectorAll('input[type="radio"]');
-border.forEach((label, index) => {
-  label.addEventListener("change", function (e) {
-    for (let i = 0; i < border.length; i++) {
-      boutonsRadioDiv[i].style.border = "1px solid #bbb8b8";
-      boutonsRadioDiv[i].style.fontWeight = "400";
-      boutonsRadioDiv[i].style.color = "";
-    }
-    if (border[index].checked != false) {
-      boutonsRadioDiv[index].style.border = "1px solid #028A3D";
-      boutonsRadioDiv[index].style.color = "#028A3D";
-      boutonsRadioDiv[index].style.fontWeight = "600";
-    }
-  });
-});
+function border(index) {
+  for (let i = 0; i < selected.length; i++) {
+    reset();
+  }
+  if (selected[index].checked != false) {
+    boutonsRadioDiv[index].style.border = "1px solid #028A3D";
+    boutonsRadioDiv[index].style.color = "#028A3D";
+    boutonsRadioDiv[index].style.fontWeight = "600";
+  }
+}
 
 // Cette fonction vérifie si une option est cochée pour activer le bouton suivant
 
@@ -64,14 +142,6 @@ function check() {
 selected.forEach((radio, idx) => {
   radio.addEventListener("change", function (e) {
     check();
+    border(idx);
   });
-});
-
-document.getElementById("quitter").addEventListener("click", function (e) {
-  e.preventDefault();
-  console.log(border);
-  console.log(border[0].checked);
-  console.log(border[1].checked);
-  console.log(border[2].checked);
-  console.log(border[3].checked);
 });
